46ece57f1fe95ed4d12e676d3a58b48ae5f6cf88
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-05-02 17:25:36 -0400

    fix setup

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -2,11 +2,13 @@
 
 # Update package lists and install required applications
 apt-get update
-apt-get install -y make golang-go git curl wget
+apt-get install -y make wget
 
 # Install the extended Hugo version
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
-apt install ./hugo.deb
+HUGO_VERSION=0.84.0
+wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
+dpkg -i hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
+rm hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 
 # Run the build command to generate the website
 make build
